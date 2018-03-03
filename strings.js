5ff08f76987c8120711adbb2c625c26dedb59185
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-03-02 19:50:33 -0600

    index and js

---
+++ strings.js
@@ -0,0 +1,18 @@
+console.log("strings.js is loading");
+
+
+function reversal() {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
